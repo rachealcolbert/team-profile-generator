a69355917127a5ceac5c564a25072fabf83d49b3
Racheal Colbert <rachealcolbert16@gmail.com>
2020-11-29 15:04:22 -0700

    intern test passed

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,11 @@
 const Intern = require('../lib/Intern')
+const Employee = require('../lib/Employee.js')
+
+
+test('creates a new intern object', () => {
+    const intern = new Intern('Racheal', 1, 'racheal@gmail.com', 'UofA');
+    expect(intern.name).toBe('Racheal');
+    expect(intern.id).toEqual(expect.any(Number));
+    expect(intern.email).toBe('racheal@gmail.com');
+    expect(intern.school).toBe('UofA');
+});
