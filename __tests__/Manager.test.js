8ee622bb41fab9323c0d57ca52d6c60b7573e8cb
Racheal Colbert <rachealcolbert16@gmail.com>
2020-11-29 14:18:44 -0700

    manager test help

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -3,7 +3,7 @@ const Manager = require('../lib/Manager.js');
 test('creates a new manager object', () => {
     const manager = new Manager('Racheal');
     expect(manager.name).toBe('Racheal');
-    expect(manager.ID).toEqual(expect.any(Number));
-    expect(manager.email).toBe(expect.any(String));
+    expect(manager.id).toEqual(expect.any(Number));
+    expect(manager.email).toBe(expect.any(Object));
     expect(manager.office).toEqual(expect.any(Number));
 });
