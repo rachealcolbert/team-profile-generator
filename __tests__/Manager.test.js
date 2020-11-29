4f511fcf4f4790f00bfdf1f62845cf89acee61ac
Racheal Colbert <rachealcolbert16@gmail.com>
2020-11-29 14:38:39 -0700

    manager object test passed

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,9 +1,10 @@
 const Manager = require('../lib/Manager.js');
+const Employee = require('../lib/Employee.js')
 
 test('creates a new manager object', () => {
-    const manager = new Manager('Racheal');
+    const manager = new Manager('Racheal', 1, 'racheal@gmail.com', 2);
     expect(manager.name).toBe('Racheal');
     expect(manager.id).toEqual(expect.any(Number));
-    expect(manager.email).toBe(expect.any(Object));
-    expect(manager.office).toEqual(expect.any(Number));
+    expect(manager.email).toBe('racheal@gmail.com');
+    expect(manager.officeNumber).toEqual(expect.any(Number));
 });
