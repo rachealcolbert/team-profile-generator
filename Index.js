4ae6f887dbec7ac80084b3355e3379e86ec20965
Racheal Colbert <rachealcolbert16@gmail.com>
2020-11-29 11:54:24 -0700

    set up html generate function

--- Index.js
+++ Index.js
@@ -1,1 +1,20 @@
-test
+const inquirer = require('inquirer');
+const fs = require('fs)');
+const generateMarkdown = require('./dist/generateMarkdown.html');
+const path = require('path');
+
+// function to write html file
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data)
+}
+// function to initialize program
+function init() {
+    questions()
+        .then(answers => {
+            writeToFile('dist/page.html', generateMarkdown({
+                ...answers
+            }));
+        })
+}
+
+new Employee().initializePortfolio();
