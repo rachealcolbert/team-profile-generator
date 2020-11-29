62effe4f6e2fcebbbd57b59fa1880a96c5a0bc06
Racheal Colbert <rachealcolbert16@gmail.com>
2020-11-29 14:59:09 -0700

    engineer object test passed

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,11 @@
-const Engineer = require('../lib/Engineer')
+const Employee = require('../lib/Employee.js')
+const Engineer = require('../lib/Engineer.js');
+
+
+test('creates a new engineer object', () => {
+    const engineer = new Engineer('Racheal', 1, 'racheal@gmail.com', 'https://github.com/rachealcolbert/');
+    expect(engineer.name).toBe('Racheal');
+    expect(engineer.id).toEqual(expect.any(Number));
+    expect(engineer.email).toBe('racheal@gmail.com');
+    expect(engineer.gitHubUsername).toBe('https://github.com/rachealcolbert/');
+});
